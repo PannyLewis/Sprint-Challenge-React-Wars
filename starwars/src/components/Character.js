31ebe453d6d397a850fb2babd8fb382babbc5a1d
Panny Lewis <pannylewis@gmail.com>
2020-05-30 13:17:37 -0500

    app.js:  added states, added .map.  character.js:  transferred the return of useeffect to character component, added styles, but props don't work

--- starwars/src/components/Character.js
+++ starwars/src/components/Character.js
@@ -1,1 +1,24 @@
 // Write your Character component here
+import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  border: 1px solid black;
+  border-radius: 6px;
+  background-color: yellow;
+  font-size: 1.8rem;
+  width: 50%;
+  margin: 2.5% auto;
+`;
+
+const Character = (props) => {
+  return (
+    <StyledDiv key={props.id}>
+      <h2>{props.name}</h2>
+      <p>{props.status}</p>
+      <img src={props.image} alt="character image" />
+    </StyledDiv>
+  );
+};
+
+export default Character;
