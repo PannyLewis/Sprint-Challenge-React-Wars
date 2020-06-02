01dc7f674fd41b15ad49129fb0552feedffebec1
Panny Lewis <pannylewis@gmail.com>
2020-06-01 19:25:51 -0500

    character={character}

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -27,7 +27,7 @@ const App = () => {
   return (
     <div className="App">
       {characters.map((character, index) => {
-        return <Character key={index} character={characters} />;
+        return <Character key={index} character={character} />;
       })}
     </div>
   );
